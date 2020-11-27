decd4700cc5ed31c9db880a04f00828be1a87c6e
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-27 19:07:16 +0000

    adding an outer div with a classname for styling

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
+import './Task.css'
+
 const Task = ({ task, ind, handleRemoveTask }) => {
     return (
-      <>
+      <div className="task">
         <div>{task}</div>
-       <button type="button" onClick = {() => handleRemoveTask(ind)}>
-          Remove Task
+        <button type="button" onClick = {() => handleRemoveTask(ind)}>
+          Remove
         </button>
-      </>
+      </div>
     )
   }
 
