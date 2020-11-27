7a577bc26eb4ebf7fce6519e8fb1de4f8fbdf7af
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-27 10:19:36 +0000

    code to add & remove a list item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+
+    input: "",
+    list: []
+  }
+
+  handleInput = (event) => {
+      this.setState({
+        input: event.target.value
+      });
+  }
+
+  handleAdd = (event) => {
+    this.setState({
+      list: [...this.state.list, this.state.input],
+      input: ""
+    });
+    event.preventDefault();
+
+  }
+
+  handleRemove = (index) => {
+    let updatedArray = this.state.list;
+    updatedArray.splice(index, 1)
+      this.setState({
+        list: updatedArray
+      });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <>
+          <Form 
+            value={this.state.input} 
+            handleAdd={this.handleAdd} 
+            handleInput={this.handleInput}
+            />
+          {this.state.list.map((item, ind) => {
+            return (
+             <ListItem key={ind} task={item} handleRemove={this.handleRemove}/>
+            )
+          })
+
+          }
+        </>
+      </div>
+    );
+  }
+  
 }
+
+const Form = ({value, handleAdd, handleInput}) => {
+ return ( <form onSubmit={handleAdd} action="">
+    <input value={value} onChange={handleInput} type="text"/>
+    <button type="submit">Add Task</button>
+  </form>
+ )
+}
+
+// const List = () => {
+//   return(
+//     <ListItem key={ind} task={item}/>
+//   )
+// }
+
+const ListItem = ({ task, ind, handleRemove }) => {
+  return (
+    <>
+      <div>{task}</div>
+     <button type="button" onClick = {() => handleRemove(ind)}>
+        Remove Task
+      </button>
+    </>
+  )
+}
+
 
 export default App;
