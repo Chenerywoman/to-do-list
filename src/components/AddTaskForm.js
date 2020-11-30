5d66f1044cb86d03c5d609b8a12069031c57b274
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-30 13:38:59 +0000

    removing console log & unnecessary id

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,9 +1,7 @@
 const AddTaskForm = ({value, handleInput, listIndex, handleAddTask}) => {
-  console.log(`value in AddTaskForm ${value}`)
-  // try adding list index to state on input & use that on submit to add task in right list
     return ( 
       <form id={listIndex} onSubmit={handleAddTask} action="">
-       <input id={listIndex} value={value} onChange={handleInput} type="text"/>
+       <input value={value} onChange={handleInput} type="text"/>
        <button type="submit">Add Task</button>
       </form>
     )
