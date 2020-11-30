033a9e9a187535d5583b9699e77846d231c6d44d
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-30 13:40:06 +0000

    adding input back to state & handleInput & amended addTask methods

--- src/components/List.js
+++ src/components/List.js
@@ -3,34 +3,57 @@ import AddTaskForm from './AddTaskForm';
 import Task from './Task';
 import './List.css'
 
-const List = ({listIndex, tasks, title, handleRemoveList, input, handleInput, handleAddTask, handleRemoveTask}) => {
-    console.log(`list in List${tasks}`);
+// const List = ({listIndex, tasks, title, handleRemoveList, input, handleInput, handleAddTask, handleRemoveTask}) => {
+
+ class List extends React.Component { 
+
+  state = {
+    input: "",
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      input: event.target.value
+    });
+  }
+    
+  handleAddTask = (event) => {
+    this.props.handleAddTaskToList(this.state.input, this.props.listIndex)
+    this.setState({
+      input: ""
+    })
+    event.preventDefault();
+
+  }
+
+  render (){
   return (
     <div className="list">
-      <h2>{title}</h2>
+      <h2>{this.props.title}</h2>
       <AddTaskForm
-        value={input}
-        handleInput={handleInput}
-        listIndex={listIndex}
-        handleAddTask={handleAddTask}
+        value={this.state.input}
+        handleInput={this.handleInput}
+        listIndex={this.props.listIndex}
+        handleAddTask={this.handleAddTask}
       />
-      {tasks.map((item, ind) => {
-        console.log(`in individual list map`)
+      {this.props.tasks.map((item, ind) => {
         return (
           <Task 
             key={ind} 
             task={item} 
-            listIndex={listIndex}
-            handleRemoveTask={handleRemoveTask} 
+            ind={ind}
+            listIndex={this.props.listIndex}
+            handleRemoveTask={this.props.handleRemoveTask} 
           />
           )
         })
       }
-      <button id="delete-list" type="button" onClick = {() => handleRemoveList(listIndex)}>
+      <button id="delete-list" type="button" onClick = {() => this.props.handleRemoveList(this.props.listIndex)}>
       Delete List
     </button>
     </div>
   )
+    }
 }
 
   export default List;
