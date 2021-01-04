be3521925597a2a86b397898a74862cc6742310b
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-04 16:01:13 +0000

    adding required to input

--- src/components/AddListForm.js
+++ src/components/AddListForm.js
@@ -4,7 +4,7 @@ const AddListForm = ({listName, handleAddList, handleListName}) => {
   
     return ( 
       <form className="form add-list" onSubmit={handleAddList} action="">
-       <input className="input add-list" value={listName} onChange={handleListName} type="text"/>
+       <input required className="input add-list" value={listName} onChange={handleListName} type="text"/>
        <button className="button add-list"type="submit" >Add list</button>
       </form>
     )
--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -3,7 +3,7 @@ import './AddTaskForm.css'
 const AddTaskForm = ({value, handleInput, listIndex, handleAddTask}) => {
     return ( 
       <form className="form add-task" id={listIndex} onSubmit={handleAddTask} action="">
-       <input className="input add-task" value={value} onChange={handleInput} type="text"/>
+       <input required className="input add-task" value={value} onChange={handleInput} type="text"/>
        <button className="button add-task" type="submit">Add</button>
       </form>
     )
